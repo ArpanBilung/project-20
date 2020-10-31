7a979e07df433276f42238256428650f5071551c
ArpanBilung <72871448+ArpanBilung@users.noreply.github.com>
2020-10-31 23:06:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,53 @@
+var car,wall;
+var speed,weight;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,400);
+
+speed = random(55,90)
+weight = random(400,1500)
+
+speed.X=20;
+
+car =createSprite(50,200,20,20);
+car.velocityX = speed;
+car.shapeColor ="red";
+car.debug = true;
+
+wall=createSprite(800,200,60,height/2);
+wall.shapeColor="grey";
+// wall.debug = true;
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
   drawSprites();
+  
+if(wall.x-car.x<(car.width+wall.width)){
+  car.velocityX=0;
+  var deformation=0.5*width*speed/22509;
+
+   
+   if(deformation<180 && deformation>100){
+     car.shapeColor=color(230,230,0)
+    } 
+         if(deformation>180){
+         car.shapeColor=color(255,0,0)
+        }
+
+    if (car.x - wall.x < wall.width/2 + car.width/2
+      && wall.x - car.x <wall .width/2 + car.width/2){
+      
+    return true;
+  }
+   else {
+   return false
+
+  }
 }
+  
+
+}
+
+
